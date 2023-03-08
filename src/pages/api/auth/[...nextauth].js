60995531097e68fb6ec69aba87c9bcf7b2c123c1
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-08 16:04:05 +0900

    Feat: Install Tailwind CSS

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,52 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
 
-export default NextAuth({
+const settings = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+  callbacks: {
+    async signIn(user, account, profile) {
+      if (account.provider === "google") {
+        const { accessToken, idToken } = account;
+
+        try {
+          const response = await axios.post(
+            `${process.env.DJANGO_URL}/api/auth/google/`,
+            { access_token: accessToken, id_token: idToken }
+          );
+          const { access_token } = response.data;
+          user.accessToken = access_token;
+
+          return true;
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return false;
+    },
+    async jwt(token, user, account, profile, isNewUser) {
+      if (user) {
+        const { accessToken } = user;
+
+        token.accessToken = accessToken;
+      }
+
+      return token;
+    },
+    async session(session, user) {
+      session.accessToken = user.accessToken;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  }
+};
+
+export default (req, res) => NextAuth(req, res, settings);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Login() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   return (
     <>
-      {!session ? (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-          <pre>{!session && "User is not logged in"}</pre>
-        </>
-      ) : (
+      {session && session.accessToken ? (
         <>
           Signed in as {session.user?.email} <br />
           <button onClick={() => signOut()}>Sign out</button>
           {session.accessToken && <pre>User has access token</pre>}
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+          <pre>{!session && "User is not logged in"}</pre>
         </>
       )}
     </>
