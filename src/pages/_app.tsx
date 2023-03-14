1616e7e99b9f1af8d13c82667aca77142c2b33b7
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-14 16:22:10 +0900

    Feat: Initialize loading state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,40 @@
 import "@/styles/globals.css";
+import React, { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Layout from "@/components/layout/layout";
 import { SessionProvider } from "next-auth/react";
+import { Router } from "next/router";
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <Layout>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <Component {...pageProps} />
+        {loading ? <h1>App is loading...</h1> : <Component {...pageProps} />}
       </Layout>
     </SessionProvider>
   );
