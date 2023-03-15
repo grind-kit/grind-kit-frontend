85083aecd2a64ea48aead845692f10699d7da5c2
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-15 11:58:20 +0900

    Feat: Implement InstanceContent fetch via context

--- src/pages/dungeons/[dungeonId].tsx
+++ src/pages/dungeons/[dungeonId].tsx
@@ -1,11 +1,24 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 const DungeonPage: NextPage = () => {
   const router = useRouter();
   const { dungeonId } = router.query;
-  
   return <h1>This is Dungeon {dungeonId}</h1>;
 };
 
 export default DungeonPage;
+
+export async function getServerSideProps(context: any) {
+  const id = context.query.dungeonId;
+  const res = await axios.get(`https://xivapi.com/InstanceContent/${id}`);
+
+  console.log(res);
+
+  return {
+    props: {
+      initialResults: res.data.Results,
+    },
+  };
+}
