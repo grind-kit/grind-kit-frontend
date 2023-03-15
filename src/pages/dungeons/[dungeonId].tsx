8d77c3faa627db5d749eb66aef5bdd73d7b3305b
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-15 14:34:51 +0900

    Refactor: Add queries to dungeonId fetch

--- src/pages/dungeons/[dungeonId].tsx
+++ src/pages/dungeons/[dungeonId].tsx
@@ -2,23 +2,43 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import axios from "axios";
 
-const DungeonPage: NextPage = () => {
-  const router = useRouter();
-  const { dungeonId } = router.query;
-  return <h1>This is Dungeon {dungeonId}</h1>;
+type TResults = {
+  Name: string;
+  Description: string;
+  Banner: string;
+  InstanceClearExp: number;
+  InstanceClearGil: number;
+  ContentType: {
+    IconHD: string;
+  };
+  ItemLevelRequired: number;
 };
 
-export default DungeonPage;
+type TProps = {
+  initialResults: TResults;
+};
+
+export default function DungeonPage({ initialResults }: TProps) {
+  const router = useRouter();
+  const { dungeonId } = router.query;
+  return (
+    <div>
+      <p>Dungeon #{dungeonId}</p>
+      <p>Name: {initialResults.Name}</p>
+      <p>Description {initialResults.Description}</p>
+    </div>
+  );
+}
 
 export async function getServerSideProps(context: any) {
   const id = context.query.dungeonId;
-  const res = await axios.get(`https://xivapi.com/InstanceContent/${id}`);
-
-  console.log(res);
+  const res = await axios.get(
+    `https://xivapi.com/InstanceContent/${id}?private_key=${process.env.XIVAPI_KEY}&language=en&columns=Name,Description,Banner,InstanceClearExp,InstanceClearGil,ContentType.IconHD,ItemLevelRequired`
+  );
 
   return {
     props: {
-      initialResults: res.data.Results,
+      initialResults: res.data,
     },
   };
 }
