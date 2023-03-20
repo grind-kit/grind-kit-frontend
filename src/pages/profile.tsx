6695ca8031a67fd3f5e95ef405c1a54de32e141a
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-20 14:35:52 +0900

    Feat: Touch signup page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,33 +1,14 @@
 import { getSession } from "next-auth/react";
 import Image from "next/image";
 
-export default function Profile({ user }: any) {
+export default function Profile() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-3xl mb-3 font-bold">Profile</h1>
-      <div className="border w-14 mb-5"></div>
-      <Image alt="User image" src={user.image} className="rounded-full mb-3" />
-      <p className="text-xl">{user.name}</p>
-      <p>{user.email}</p>
+      <p>Sign-in was successful</p>
     </div>
   );
 }
 
-export async function getServerSideProps(context: any) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context: any) {
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      user: session.user,
-    },
-  };
-}
+// }
