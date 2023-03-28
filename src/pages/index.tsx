191cd0674a74cb2296fca18fd3b8bc033e2cb3be
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-28 10:45:03 +0900

    Feat: Set cookies on index.tsx landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import { useAuth } from "@/context/AuthContext";
+import { auth } from "@/firebase/firebase";
 
 export default function Home() {
+  const { user } = useAuth();
+  const token = auth.currentUser?.getIdToken();
+
+  useEffect(() => {
+    document.cookie = `token=${token}; path=/`;
+    document.cookie = `uid=${user.uid}; path=/`;
+  }, [user, token]);
+
   return (
     <div>
       <Head>
