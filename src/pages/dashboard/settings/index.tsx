ceef77b81944ab3b6b7bbce20f58ab190b5b7aa5
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-28 18:00:44 +0900

    Refactor: Slice non-active servers from response list

--- src/pages/dashboard/settings/index.tsx
+++ src/pages/dashboard/settings/index.tsx
@@ -1,33 +1,55 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
-import React, { useState } from "react";
+import React, { useState, useEffect, FormEventHandler } from "react";
 import axios from "axios";
 
 export default function SettingsPage({ servers }: { servers: string[] }) {
-  const [characterName, setCharacterName] = useState<string>("");
-  const [server, setServer] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [selectedServer, setSelectedServer] = useState<string>("");
 
-  const handleSubmit = () => {};
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setName(value);
+    console.log(name);
+  };
+
+  const handleServer = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setSelectedServer(value);
+    console.log(selectedServer);
+  };
+
+  const handleSearch = () => {
+    console.log(name, selectedServer);
+  };
 
   return (
     <ProtectedRoute>
       <h2>Settings</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <label htmlFor="name">Name</label>
         <input
           inputMode="text"
           id="name"
           name="name"
-          value={characterName}
-          onChange={(e) => setCharacterName(e.target.value)}
+          value={name}
+          onChange={handleName}
         />
         <label htmlFor="server">Server</label>
-        <select id="server" name="server">
+        <select
+          id="server"
+          name="server"
+          value={selectedServer}
+          onChange={handleServer}
+        >
           {servers.map((server: string) => (
             <option key={server} value={server}>
               {server}
             </option>
           ))}
         </select>
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
       </form>
     </ProtectedRoute>
   );
@@ -38,9 +60,11 @@ export const getServerSideProps = async () => {
     `https://xivapi.com/servers?private_key=${process.env.XIVAPI_KEY}`
   );
 
+  const slicedArrayOfServers = response.data.slice(0, 77);
+
   return {
     props: {
-      servers: response.data,
+      servers: slicedArrayOfServers,
     },
   };
 };
