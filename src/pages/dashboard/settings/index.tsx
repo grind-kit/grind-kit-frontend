4d239f82f6842245175dbcb9fc3a7b3778716c02
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-03-29 14:44:07 +0900

    Feat: Initialize handleSave function on settings page

--- src/pages/dashboard/settings/index.tsx
+++ src/pages/dashboard/settings/index.tsx
@@ -1,11 +1,33 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
-import React, { useState, useEffect, FormEventHandler } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { parseCookies } from "nookies";
 
-export default function SettingsPage({ servers }: { servers: string[] }) {
+type TCharacter = {
+  Avatar: string;
+  FeastMatches: number;
+  ID: number;
+  Lang: string;
+  Name: string;
+  Rank: number | null;
+  RankIcon: string | null;
+  Server: string;
+};
+
+type TSettingsPageProps = {
+  servers: string[];
+  token: string | null;
+  uid: string | null;
+};
+
+export default function SettingsPage({
+  servers,
+  token,
+  uid,
+}: TSettingsPageProps) {
   const [name, setName] = useState<string>("");
   const [selectedServer, setSelectedServer] = useState<string>("");
-  const [character, setCharacter] = useState<any>(null);
+  const [character, setCharacter] = useState<TCharacter | null>(null);
 
   const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -21,13 +43,26 @@ export default function SettingsPage({ servers }: { servers: string[] }) {
     const response = await axios.get(
       `https://xivapi.com/character/search?name=${name}&server=${selectedServer}&private_key=${process.env.XIVAPI_KEY}`
     );
-    console.log(response.data);
     setCharacter(response.data.Results[0]);
   };
 
-  useEffect(() => {
-    console.log(character);
-  }, [character]);
+  const handleSave = async () => {
+    try {
+      const response = await axios.put(
+        `${process.env.BACKEND_URL}/users/${uid}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: {
+            lodestone_id: character?.ID,
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <ProtectedRoute>
@@ -58,20 +93,30 @@ export default function SettingsPage({ servers }: { servers: string[] }) {
           Search
         </button>
       </form>
+      {character && (
+        <div onClick={handleSave}>
+          <h3>{character.Name}</h3>
+          <img src={character.Avatar} alt={character.Name} />
+        </div>
+      )}
     </ProtectedRoute>
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context: any) => {
   const response = await axios.get(
     `https://xivapi.com/servers?private_key=${process.env.XIVAPI_KEY}`
   );
 
-  const slicedArrayOfServers = response.data.slice(0, 77);
+  const servers = response.data.slice(0, 77);
+
+  const { uid, token } = parseCookies(context);
 
   return {
     props: {
-      servers: slicedArrayOfServers,
+      servers,
+      uid,
+      token,
     },
   };
 };
