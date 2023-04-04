8f0de4de704bb66682aa73234acaa6d574cc70ea
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-04-04 09:41:57 +0900

    Feat: Implement arrayOfClassJobs fetching in jobs/index.tsx

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { RoleAccordion } from "@/components/Accordion";
 import { roleData } from "@/data";
+import { parseCookies } from "nookies";
+import axios from "axios";
+import { User } from "@/pages/api/api-client";
+import { TClassJob } from "types/global";
 
-export default function Jobs() {
+export default function JobsPage(arrayOfClassJobs: TClassJob) {
   return (
     <ProtectedRoute>
       <div className="w-full flex flex-col items-center">
@@ -14,4 +18,26 @@ export default function Jobs() {
       </div>
     </ProtectedRoute>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const { uid, token } = parseCookies(context);
+  const clientRes = await User.getUserInfo(uid, token);
+  let arrayOfClassJobs = null;
+
+  if (clientRes.lodestone_id !== null) {
+    const handlerRes = await axios.get(
+      `https://xivapi.com/character/${clientRes.lodestone_id}`
+    );
+
+    arrayOfClassJobs = handlerRes.data.Character.ClassJobs.slice(0, 19);
+  }
+
+  console.log(arrayOfClassJobs);
+
+  return {
+    props: {
+      arrayOfClassJobs: arrayOfClassJobs,
+    },
+  };
 }
