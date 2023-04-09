f0885bc449d09d33c130daa3b765d0323faae7e2
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-04-09 13:07:49 +0900

    Refactor: Convert settings form to useForm React hook

--- src/pages/dashboard/settings/index.tsx
+++ src/pages/dashboard/settings/index.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 import { User } from "@/pages/api/api-client";
 import Image from "next/image";
+import { FormProvider, useForm } from "react-hook-form";
 
 type TCharacter = {
   Avatar: string;
@@ -22,28 +23,28 @@ type TSettingsPageProps = {
   uid: string;
 };
 
+interface SearchType {
+  characterName: string;
+  server: string;
+}
+
 export default function SettingsPage({
   servers,
   token,
   uid,
 }: TSettingsPageProps) {
-  const [name, setName] = useState<string>("");
-  const [selectedServer, setSelectedServer] = useState<string>("");
+  const methods = useForm<SearchType>({ mode: "onBlur" });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
   const [character, setCharacter] = useState<TCharacter | null>(null);
 
-  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setName(value);
-  };
-
-  const handleServer = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value;
-    setSelectedServer(value);
-  };
-
-  const handleSearch = async () => {
+  const onSubmit = async (data: SearchType) => {
+    const { characterName, server } = data;
     const response = await axios.get(
-      `https://xivapi.com/character/search?name=${name}&server=${selectedServer}&private_key=${process.env.XIVAPI_KEY}`
+      `https://xivapi.com/character/search?name=${characterName}&server=${server}&private_key=${process.env.XIVAPI_KEY}`
     );
     setCharacter(response.data.Results[0]);
   };
@@ -58,60 +59,69 @@ export default function SettingsPage({
         <h2 className="px-12 mt-8 text-center text-2xl font-semibold text-blue-500">
           Settings
         </h2>
-        <form action="" className="w-80 mx-auto pb-12 px-4">
-          <div className="mt-8">
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="name"
-                className="block mb-3 font-sans text-blue-500"
-              >
-                Character Name
-              </label>
-            </div>
+        <FormProvider {...methods}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-80 mx-auto pb-12 px-4"
+          >
+            <div className="mt-8">
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="characterName"
+                  className="block mb-3 font-sans text-blue-500"
+                >
+                  Character Name
+                </label>
+              </div>
 
-            <input
-              inputMode="text"
-              id="name"
-              name="name"
-              value={name}
-              className={`border border-solid rounded-md ring:0 focus:ring-0 focus:outline-none border-gray-200 text-slate-900 text-normal py-3 h-12 px-6 text-lg w-full flex items-center`}
-              onChange={handleName}
-            />
-          </div>
-          <div className="mt-8">
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="server"
-                className="block mb-3 font-sans text-blue-500"
-              >
-                Server
-              </label>
+              <input
+                inputMode="text"
+                id="characterName"
+                className={`border border-solid rounded-md ring:0 focus:ring-0 focus:outline-none border-gray-200 text-slate-900 text-normal py-3 h-12 px-6 text-lg w-full flex items-center`}
+                {...register("characterName", {
+                  required: "Name is required",
+                  minLength: { value: 4, message: "Name is too short" },
+                  maxLength: { value: 20, message: "Name is too long" },
+                })}
+              />
+              {errors.characterName && (
+                <p className="text-red-500">{errors.characterName.message}</p>
+              )}
             </div>
+            <div className="mt-8">
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="server"
+                  className="block mb-3 font-sans text-blue-500"
+                >
+                  Server
+                </label>
+              </div>
 
-            <select
-              id="server"
-              name="server"
-              value={selectedServer}
-              className={`border border-solid rounded-md ring:0 focus:ring-0 focus:outline-none border-gray-200 text-slate-900 text-normal py-3 h-12 px-6 text-lg w-full flex items-center`}
-              onChange={handleServer}
-            >
-              {servers.map((server: string) => (
-                <option key={server} value={server}>
-                  {server}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="flex justify-center pt-8">
-            <button
-              type="button"
-              onClick={handleSearch}
-              className={`h-12 text-center w-2/3 bg-blue-500 border-2 rounded-md hover:shadow-lg hover:bg-blue-400 text-lg transition`}
-            >
-              <p className="capitalize text-white font-normal">Search</p>
-            </button>
-          </div>
-        </form>
+              <select
+                id="server"
+                className={`border border-solid rounded-md ring:0 focus:ring-0 focus:outline-none border-gray-200 text-slate-900 text-normal py-3 h-12 px-6 text-lg w-full flex items-center`}
+                {...register("server", {
+                  required: "Server is required",
+                })}
+              >
+                {servers.map((server: string) => (
+                  <option key={server} value={server}>
+                    {server}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-center pt-8">
+              <button
+                type="submit"
+                className={`h-12 text-center w-2/3 bg-blue-500 border-2 rounded-md hover:shadow-lg hover:bg-blue-400 text-lg transition`}
+              >
+                <p className="capitalize text-white font-normal">Search</p>
+              </button>
+            </div>
+          </form>
+        </FormProvider>
         {character && (
           <div className="hover:cursor-pointer" onClick={handleSave}>
             <h3>{character.Name}</h3>
