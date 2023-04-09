7f0b8ff7911db0ef73cef88bb5f4cb9b7d58c6f0
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-04-09 10:25:54 +0900

    Refactor: Convert Firebase env variables to non-client facing

--- process.d.ts
+++ process.d.ts
@@ -1,11 +1,11 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NEXT_PUBLIC_FIREBASE_API_KEY: string | undefined;
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string | undefined;
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string | undefined;
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string | undefined;
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string | undefined;
-    NEXT_PUBLIC_FIREBASE_APP_ID: string | undefined;
+    FIREBASE_API_KEY: string | undefined;
+    FIREBASE_AUTH_DOMAIN: string | undefined;
+    FIREBASE_PROJECT_ID: string | undefined;
+    FIREBASE_STORAGE_BUCKET: string | undefined;
+    FIREBASE_MESSAGING_SENDER_ID: string | undefined;
+    FIREBASE_APP_ID: string | undefined;
 
     XVIAPI_KEY: string | undefined;
 
