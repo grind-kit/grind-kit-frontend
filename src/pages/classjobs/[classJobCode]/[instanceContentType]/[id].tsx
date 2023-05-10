d6ba57551dc1a7c255c0a957d251e6a9606a6a07
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-05-10 17:45:08 +0900

    Refactor: Correct HTML to be semantic on IdPage

--- src/pages/classjobs/[classJobCode]/[instanceContentType]/[id].tsx
+++ src/pages/classjobs/[classJobCode]/[instanceContentType]/[id].tsx
@@ -20,74 +20,81 @@ export default function IdPage({
 }: TIdPage) {
   const strings = loadStrings;
 
-  function handleImage(src: string) {
+  function handleImage(src: string): string {
     return `${process.env.XIVAPI_URL}` + src;
   }
 
   return (
     <ProtectedRoute>
-      <div className="w-full flex flex-col items-center">
-        <h2 className="text-3xl font-bold text-slate-900">
-          {strings.DETAILS_HEADER}
-        </h2>
-        <div className="mt-5 grid grid-cols-2 gap-5">
-          <div className="w-full flex flex-col bg-gray-200 p-4 rounded-md">
-            <span className="text-slate-900 uppercase font-bold tracking-wide text-center">
-              {typeName}
-            </span>
-            <div className="flex flex-row justify-center gap-5 mt-5">
+      <table className="text-slate-900">
+        <thead className="w-full flex flex-col items-center">
+          <tr>
+            <th colSpan={2} className="w-full">
+              <h2 className="text-3xl font-bold">{strings.DETAILS_HEADER}</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="mt-5 grid grid-cols-2 gap-5">
+            <td className="w-full flex flex-col bg-gray-200 p-4 rounded-md">
+              <header className="flex flex-col items-center font-bold">
+                <h3 className="uppercase tracking-wide">{typeName}</h3>
+                <div className="flex flex-row justify-center gap-5 mt-5">
+                  <Image
+                    src={handleImage(typeIcon)}
+                    alt="The categorical icon for the instance content type"
+                    width={32}
+                    height={32}
+                  />
+                  <h4 className="capitalize text-2xl">{name}</h4>
+                </div>
+              </header>
               <Image
-                src={handleImage(typeIcon)}
-                alt="The icon for the instance content type"
-                width={32}
-                height={32}
+                className="mt-5 mx-auto"
+                src={handleImage(banner)}
+                alt="The banner image for the instance content"
+                width={376}
+                height={120}
               />
-              <h3 className="capitalize text-2xl font-bold text-slate-900">
-                {name}
-              </h3>
-            </div>
-            <Image
-              className="mt-5 mx-auto"
-              src={handleImage(banner)}
-              alt="The banner image for the instance content"
-              width={376}
-              height={120}
-            />
-            <div className="mt-5 grid grid-cols-2 grid-rows-2">
-              <h4 className="text-slate-900 text-xl uppercase font-bold tracking-wide">
-                {strings.DETAILS_REQUIREMENTS_LABEL}
-              </h4>
-              <div className="flex flex-row gap-3">
-                <span className="text-slate-900 capitalize font-bold tracking-wide">
-                  {strings.DETAILS_LEVEL_LABEL} {classJobLevel}
+
+              <section className="mt-5 flex flex-col items-center">
+                <h5 className="text-xl uppercase font-bold tracking-wide">
+                  {strings.DETAILS_REQUIREMENTS_LABEL}
+                </h5>
+                <span className="capitalize font-bold tracking-wide">
+                  {strings.DETAILS_LEVEL_LABEL + classJobLevel}
                 </span>
-                <span className="text-slate-900 capitalize font-bold tracking-wide">
-                  {strings.DETAILS_ILVL_LABEL} {itemLevel}
+                <span className="capitalize font-bold tracking-wide">
+                  {strings.DETAILS_ILVL_LABEL + itemLevel}
                 </span>
-              </div>
-              <h4 className="text-slate-900 text-xl uppercase font-bold tracking-wide">
-                {strings.DETAILS_LOCATION_LABEL}
-              </h4>
-              <div className="flex flex-row gap-3">
-                <Image
-                  src={handleImage(regionIcon)}
-                  alt="The icon for the region the instance content is located in"
-                  width={32}
-                  height={32}
-                />
-                <span className="text-slate-900 capitalize font-bold tracking-wide">
-                  {regionName}
-                </span>
-              </div>
-            </div>
-            <span className="text-slate-900 uppercase font-bold tracking-wide mt-5 text-center">
-              {strings.DETAILS_DESCRIPTION_LABEL}
-            </span>
-            <p className="mt-5 text-slate-900 leading-relaxed">{description}</p>
-          </div>
-          <div>02</div>
-        </div>
-      </div>
+                <h5 className="mt-5 text-xl uppercase font-bold tracking-wide">
+                  {strings.DETAILS_LOCATION_LABEL}
+                </h5>
+                <div className="flex items-center">
+                  <Image
+                    src={handleImage(regionIcon)}
+                    alt="The icon for the region the instance content is located in"
+                    width={32}
+                    height={32}
+                  />
+                  <span className="capitalize font-bold tracking-wide">
+                    {regionName}
+                  </span>
+                </div>
+              </section>
+
+              <section className="mt-5 flex flex-col items-center">
+                <h3 className="uppercase font-bold tracking-wide">
+                  {strings.DETAILS_DESCRIPTION_LABEL}
+                </h3>
+                <p className="mt-5 leading-relaxed">{description}</p>
+              </section>
+            </td>
+
+            <td>02</td>
+          </tr>
+        </tbody>
+      </table>
     </ProtectedRoute>
   );
 }
