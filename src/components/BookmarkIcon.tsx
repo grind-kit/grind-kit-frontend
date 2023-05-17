53ab914a1aecb152b60e1b469956be7cf6e9e19d
chadgrover <93070367+chadgrover@users.noreply.github.com>
2023-05-17 15:39:02 +0900

    Style: Implement basic styling for BookmarkIcon

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -23,5 +23,27 @@ export default function BookmarkIcon({
     return response;
   }
 
-  return <button onClick={handleBookmark}>Bookmark</button>;
+  return (
+    <button
+      onClick={handleBookmark}
+      className="flex gap-x-1.5 bg-orange-500 hover:bg-orange-700 transition-all px-4 py-2 font-bold text-white rounded-full"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6 inline-block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+        />
+      </svg>
+
+      <span className="inline">Bookmark</span>
+    </button>
+  );
 }
